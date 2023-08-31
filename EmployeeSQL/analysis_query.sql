c9801ed35be5539e54f288c3741b9b230c961936
d-p1 <rodriguezdiana.p1@gmail.com>
2023-08-30 20:49:32 -0700

    table fixed

--- EmployeeSQL/analysis_query.sql
+++ EmployeeSQL/analysis_query.sql
@@ -28,7 +28,7 @@ select dept_manager.dept_no
 	, employees.first_name
 from dept_manager
 inner join departments on
-dept_mnager.dept_no = d.dept_no
+dept_manager.dept_no = d.dept_no
 inner join employees e on
 departments.emp_no = employees.emp_no;
 
@@ -54,3 +54,35 @@ from employees
 where first_name = 'Hercules'
 and last_name like 'B%';
 
+--6 List each employee in the Sales department, including their employee number, last name, and first name.
+
+select employees.emp_no
+    , employees.last_name
+    , employees.first_name
+from employees
+inner join dept_emp on
+    employees.emp_no = dept_emp.emp_no
+inner join departments on
+    departments.dept_no = dept_emp.dept_no
+where departments.dept_name = 'Sales';	
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+select employees.emp_no
+    , employees.last_name
+    , employees.first_name
+    , departments.dept_name
+	from employees
+inner join dept_emp ON
+    employees.emp_no = dept_emp.emp_no
+inner join departments on
+    departments.dept_no = dept_emp.dept_no
+where departments.dept_name in ('Sales', 'Development');
+
+--8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+select last_name,
+count(*) as frequency
+from employees
+group by last_name
+order by frequency desc;
